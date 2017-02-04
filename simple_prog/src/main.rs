525abd6a9fe99ca52fe92cdb76ed9e0d4d9e97a0
CompSciGuyIT <compsciguy.it@gmail.com>
2017-02-04 10:02:38 +1000

    simple_prog created to see: - How easy it is to set up a project from scratch and have it integrate seamlessly with Git.

---
+++ simple_prog/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("This is just a simple program.");
+    println!("I am using to see how easy it is");
+    println!("to set up a new project");
+    println!("and integrate it with Git.");
+}
